cc8fcef030b9a63673affeedbdb18b2b3bd50b6a
jeuniii <107853954+jeongdung-eo@users.noreply.github.com>
2022-08-06 20:00:37 +0900

    Add files via upload

---
+++ PHPicker-FlowLayout/PHPickerController/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PHPickerController
+//
+//  Created by 김정은 on 2022/08/06.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var phtoImageView: UIImageView!
+    
+}
